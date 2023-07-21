599fe39712570b43987738edb987ad9fd59a091f
IvanIsak2000 <vingving648@gmail.com>
2023-07-21 19:12:00 +0300

    rewrite on correct task

--- 8.py
+++ 8.py
@@ -1,37 +1,25 @@
 from alphabet import alphabet
 
-from flask import Flask, request, render_template, abort
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    letters = list(alphabet)[:5]
-    print(letters)
-    return ''.join(map(str.lower, letters))
+@app.route('/search/')
+def subtitle():
 
-@app.route('/page/<number>')
-def number(number):
-    if int(number) <= 5: 
-        letters = list(map(str.lower,list(alphabet)))
+    '''автор походу забыл указать и BRAvo в которой содердтся подстрока ra'''
+    subtitle =  request.args.get('s')
 
-        by_5 = []
-        first_absolutly_posit = 0
-        second_absolutly_posit = 5 
+    letters = list(dict(alphabet).values())
+    letters = list(map(str.lower, letters))
 
-        limit = 5
+    result = []
+    
+    for l in letters:   
+        if subtitle in l:
+            result.append(l.title())
 
-        while limit!=0:
-            by_5.append(list(letters)[first_absolutly_posit:second_absolutly_posit])
-
-            first_absolutly_posit += 5
-            second_absolutly_posit += 5
-            limit -=1
-
-        print(type(by_5))
-        return ''.join(list(by_5)[int(number)-1])
-    return abort(404)
-
-
-
-
+    if not result:
+        return abort(404)
+        
+    return ', '.join(result)
