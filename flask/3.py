4c0853acb0bf96267e7a286fce4cbfed6fa17876
IvanIsak2000 <vingving648@gmail.com>
2023-07-24 14:21:46 +0300

    alphabet as module

--- flask/3.py
+++ flask/3.py
@@ -2,39 +2,8 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
-alphabet = {
-  "A": "Alfa",
-  "B": "Bravo",
-  "C": "Charlie",
-  "D": "Delta",
-  "E": "Echo",
-  "F": "Foxtrot",
-  "G": "Golf",
-  "H": "Hotel",
-  "I": "India",
-  "J": "Juliett",
-  "K": "Kilo",
-  "L": "Lima",
-  "M": "Mike",
-  "N": "November",
-  "O": "Oscar",
-  "P": "Papa",
-  "Q": "Quebec",
-  "R": "Romeo",
-  "S": "Sierra",
-  "T": "Tango",
-  "U": "Uniform",
-  "V": "Victor",
-  "W": "Whiskey",
-  "X": "X-ray",
-  "Y": "Yankee",
-  "Z": "Zulu",
-}
+from alphabet import alphabet
 
 @app.route('/check/<letter>/<word>')
 def equals(letter, word):
-    # print(alphabet[letter.upper()])
-    # print(word.title())
-    # return word.title()
     return  str(alphabet[letter.upper()] == word.title())
