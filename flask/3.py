7799605912b96510cfe28f94b18f16fca5cf76b8
IvanIsak2000 <vingving648@gmail.com>
2023-08-06 15:54:26 +0300

    removed

--- flask/3.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-from alphabet import alphabet
-
-@app.route('/check/<letter>/<word>')
-def equals(letter, word):
-    return  str(alphabet[letter.upper()] == word.title())
