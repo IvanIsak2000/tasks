bb8e5d97ded9e27542cdde9845ad32247ded9f76
IvanIsak2000 <vingving648@gmail.com>
2023-08-21 00:36:01 +0300

    fix imports, add `app.run`

--- flask/alphabet_tasks/2.py
+++ flask/alphabet_tasks/2.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
+from alphabet import alphabet
 
 app = Flask(__name__)
 
-from alphabet import alphabet
 
 @app.route('/find/')
 def find_letter():
     letter = request.args.get('letter').upper()
     return alphabet[letter]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
