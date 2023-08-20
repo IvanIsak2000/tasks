f85c36aae58f69a24fad19bb953d2e9c23356240
IvanIsak2000 <vingving648@gmail.com>
2023-08-21 00:38:22 +0300

    fix imports, add `app.run`, fix doc strs

--- flask/alphabet_tasks/8.py
+++ flask/alphabet_tasks/8.py
@@ -4,22 +4,27 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
+
 @app.route('/search/')
 def subtitle():
+    """автор походу забыл указать и BRAvo в которой содердтся подстрока ra"""
 
-    '''автор походу забыл указать и BRAvo в которой содердтся подстрока ra'''
-    subtitle =  request.args.get('s')
+    subtitle = request.args.get('s')
 
     letters = list(dict(alphabet).values())
     letters = list(map(str.lower, letters))
 
     result = []
-    
-    for l in letters:   
+
+    for l in letters:
         if subtitle in l:
             result.append(l.title())
 
     if not result:
         return abort(404)
-        
+
     return ', '.join(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
