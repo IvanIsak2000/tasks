79d21676d1e6f3250639ae7957f2fcd380b92f41
IvanIsak2000 <vingving648@gmail.com>
2023-08-21 00:36:41 +0300

    fix imports, add `app.run`, fix vars names

--- flask/alphabet_tasks/4.py
+++ flask/alphabet_tasks/4.py
@@ -4,22 +4,26 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/between/')
 def between():
     start_letter = request.args.get('from').upper()
     end_letter = request.args.get('to').upper()
 
+    if start_letter != end_letter:
 
-    if start != end:
-
-        start_index = list(alphabet).index(start_index)
-        end_index =  list(alphabet).index(end_letter)
+        start_index = list(alphabet).index(start_letter)
+        end_index = list(alphabet).index(end_letter)
 
         if not start_index < end_index:
-            letters =  list(alphabet)[end_index+1:start_index]
+            letters = list(alphabet)[end_index + 1:start_index]
             return ''.join(letters)
 
-        letters = list(alphabet)[start_index+1:end_index]
+        letters = list(alphabet)[start_index + 1:end_index]
         return ''.join(letters)
 
     return '-'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
