bae657e87e59683ac36335cf78285f73170876da
IvanIsak2000 <vingving648@gmail.com>
2023-08-21 00:37:35 +0300

    add `app.run`, pep8 format

--- flask/alphabet_tasks/6.py
+++ flask/alphabet_tasks/6.py
@@ -4,22 +4,23 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/letters/')
 def offset():
     limit = int(request.args.get('limit'))
     offset = int(request.args.get('offset'))
     letters = list(map(str.lower, alphabet))
 
-    if limit <= 26 and offset <26:
+    if limit <= 26 and offset < 26:
         answer = []
-        i=0
-        while limit>0:
+        i = 0
+        while limit > 0:
             try:
-                answer.append(letters[offset+i].lower())
+                answer.append(letters[offset + i].lower())
             except IndexError:
                 break
-            i+=1
-            limit -=1
+            i += 1
+            limit -= 1
         return ''.join(answer)
 
     return '-'
