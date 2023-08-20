f8739a329c1900d0acbd2d021816c777814a5ac7
IvanIsak2000 <vingving648@gmail.com>
2023-08-21 00:36:12 +0300

    fix imports, add `app.run`

--- flask/alphabet_tasks/3.py
+++ flask/alphabet_tasks/3.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
+from alphabet import alphabet
 
 app = Flask(__name__)
 
-from alphabet import alphabet
 
 @app.route('/check/<letter>/<word>')
 def equals(letter, word):
-    return  str(alphabet[letter.upper()] == word.title())
+    return str(alphabet[letter.upper()] == word.title())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
