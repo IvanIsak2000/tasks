219014f9d7d2055887e4addf30123fc7fc21d731
IvanIsak2000 <vingving648@gmail.com>
2023-08-28 01:25:19 +0300

    solve 3rd task

--- flask/templates_tasks/3.py
+++ flask/templates_tasks/3.py
@@ -2,6 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+aircraft = {
+    'model': 'Airbus A380',
+    'year': 2007,
+    'crew': 3,
+    'airline': {
+        'name': 'Emirates',
+        'country': 'ОАЭ'
+    }
+}
 
 @app.route('/')
-def
+def get_template():
+    return render_template('3.html',
+                           model=aircraft['model'],
+                           name=aircraft['airline']['name'],
+                           country=aircraft['airline']['country']
+                           )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
