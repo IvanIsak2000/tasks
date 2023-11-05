51134e1287d47757e719aa02678df6a7d45f2384
IvanIsak2000 <vingving648@gmail.com>
2023-11-05 22:08:40 +0300

    change folder

--- tech_task_1/site/app1/urls.py
+++
@@ -1,6 +0,0 @@
-# from django.urls import path, re_path
-# from app1 import views
-
-# urlpatterns = [
-#     path('', views.home, name='home')
-# ]
--- tech_task_1/site/djanwiki/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from app1 import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup')
-]
