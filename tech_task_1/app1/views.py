bb81b08e4edcfe71b5c7e528cded0efe3afe90d5
IvanIsak2000 <vingving648@gmail.com>
2023-11-12 23:16:21 +0300

    add views of app

--- tech_task_1/app1/views.py
+++ tech_task_1/app1/views.py
@@ -1,18 +1,33 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
 from .models import Text
 
 
 def home(request):
-    data = Text.objects.all().filter(public=True)
+    data = Text.objects.all().filter(public=True).order_by('?')
     context = {
     'data': data
     }
     return render(request, 'app1/index.html', context)
 
-def signup(request):
-    if request.POST:
-        name = request.POST.get('name', 0)
-        password = request.POST.get('password', 0)
-        print(name, password)
-        return HttpResponse(f'{name}:{login}')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(login=cd['login'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'app1/login.html', {'form': form})
