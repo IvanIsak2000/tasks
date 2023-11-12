a4e78d826900301c2c9771374794067296c55dc4
IvanIsak2000 <vingving648@gmail.com>
2023-11-12 23:16:39 +0300

    fix main urls

--- tech_task_1/djanwiki/urls.py
+++ tech_task_1/djanwiki/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from django.contrib import admin
 from app1 import views
-
 urlpatterns = [
-    path('', views.home, name='index'),
-    path('signup', views.signup, name='signup')
+    path('', views.home, name='home'),
+    re_path(r'^account/', views.user_login),
+    re_path(r'^login/$', views.user_login, name='login'),
 ]
