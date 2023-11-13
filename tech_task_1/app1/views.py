74dc471753eb7d0d8c9d047ada80c50ef45d91b2
IvanIsak2000 <vingving648@gmail.com>
2023-11-13 22:20:42 +0300

    fix return data with login/logout

--- tech_task_1/app1/views.py
+++ tech_task_1/app1/views.py
@@ -2,11 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth import logout as auth_logout
+
 
 from .models import Text
 
 
 def home(request):
+    try:
+        auth_logout(request)
+    except:
+        pass
     data = Text.objects.all().filter(public=True).order_by('?')
     context = {
     'data': data
@@ -19,11 +25,16 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(login=cd['login'], password=cd['password'])
+            user = authenticate(username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    data = Text.objects.all().order_by('?')
+                    context = {
+                        'data': data,
+                        'username': cd['username']
+                    }
+                    return render(request, 'app1/index.html', context)
                 else:
                     return HttpResponse('Disabled account')
             else:
