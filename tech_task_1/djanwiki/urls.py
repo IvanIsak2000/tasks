a0268b96e9f65abbf887e2a4bb2d419040910d5d
IvanIsak2000 <vingving648@gmail.com>
2023-11-17 22:29:38 +0300

    comment unused code

--- tech_task_1/djanwiki/urls.py
+++ tech_task_1/djanwiki/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('admin/', admin.site.urls),
     re_path(r'^account/$', views.user_login, name='account'),
-    re_path(r'^logout', views.home, name='home'),
+    # re_path(r'^logout', views.home, name='home'),
 
 ]
